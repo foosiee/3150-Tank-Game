281114dfa99d319bc0e7b9df4eda7544c0bb76ae
foosiee <colefoos27@hotmail.com>
2020-01-29 17:45:16 -0500

    Added draw function

--- Tank.py
+++ Tank.py
@@ -10,6 +10,33 @@ class Tank(turtle.Turtle):
         self.shape(self.curr_node.val)
         self.up()
 
+    def move_up(self):
+        if self.ycor() <= 220:
+            self.sety(self.ycor()+60)
+
+    def move_down(self):
+        if self.ycor() >= -220:
+            self.sety(self.ycor()-60)
+
+    def move_left(self):
+        if self.xcor() > -350:
+            self.setx((self.xcor()-60))
+
+    def move_right(self):
+        if self.xcor() <= 350:
+            self.setx((self.xcor()+60))
+
+    def rotate_right(self):
+        self.curr_node = self.curr_node.next
+        self.draw(self.curr_node.val)
+
+    def rotate_left(self):
+        self.curr_node = self.curr_node.prev
+        self.draw(self.curr_node.val)
+
+    def draw(self, asset):
+        self.shape(asset)
+
     def __generate_list(self):
         frontNode = Node(f"Assets/{self.team}/tank.gif")
         rightNode = Node(f"Assets/{self.team}/tankright.gif")
@@ -29,27 +56,3 @@ class Tank(turtle.Turtle):
         leftNode.prev = downNode
 
         return frontNode
-
-    def move_up(self):
-        if self.ycor() <= 220:
-            self.sety(self.ycor()+60)
-
-    def move_down(self):
-        if self.ycor() >= -220:
-            self.sety(self.ycor()-60)
-
-    def move_left(self):
-        if self.xcor() > -350:
-            self.setx((self.xcor()-60))
-
-    def move_right(self):
-        if self.xcor() <= 350:
-            self.setx((self.xcor()+60))
-
-    def rotate_right(self):
-        self.curr_node = self.curr_node.next
-        self.shape(self.curr_node.val)
-
-    def rotate_left(self):
-        self.curr_node = self.curr_node.prev
-        self.shape(self.curr_node.val)
