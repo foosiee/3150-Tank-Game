28669320dcf3030390e24183e79d067ce2a0245b
foosiee <colefoos27@hotmail.com>
2020-02-10 16:33:56 -0500

    Added merge sort

--- src/TankManager.py
+++ src/TankManager.py
@@ -1,4 +1,4 @@
-from Utilites import *
+from Utilites import get_distance, get_angle, merge_sort
 
 class TankManager:
     def __init__(self):
@@ -18,15 +18,15 @@ class TankManager:
         print(s)
 
     def get_closest_tank(self, tank):
-        closest_tank = None
-        closest_distance = 9999
-        for t in self.tanks:
-            if t != tank:
-                distance = get_distance(tank, t)
-                if distance < closest_distance:
-                    closest_tank = t
-                    closest_distance = distance
-        return closest_tank
-        
+        merge_sort(self.tanks, tank)
+        return self.tanks[1] # return index 1 to skip the user tank
 
-    
+        # closest_tank = None
+        # closest_distance = 9999
+        # for t in self.tanks:
+        #     if t != tank:
+        #         distance = get_distance(tank, t)
+        #         if distance < closest_distance:
+        #             closest_tank = t
+        #             closest_distance = distance
+        # return closest_tank
--- src/Utilites.py
+++ src/Utilites.py
@@ -10,3 +10,34 @@ def get_distance(tank, tank1):
 
 def get_angle(tank, tank1):
     return math.degrees(math.asin(abs(tank1.ycor() - tank.ycor())/(abs(get_distance(tank,tank1)))))
+
+def merge_sort(tank_arr, tank):
+    if len(tank_arr) > 1:
+            mid = len(tank_arr) // 2
+            left_half = tank_arr[:mid]
+            right_half = tank_arr[mid:]
+
+            merge_sort(left_half, tank)
+            merge_sort(right_half, tank)
+
+            i = 0
+            j = 0
+            k = 0
+            while i < len(left_half) and j < len(right_half):
+                if get_distance(left_half[i], tank) <= get_distance(right_half[j], tank):
+                    tank_arr[k] = left_half[i]
+                    i+=1
+                else:
+                    tank_arr[k] = right_half[j]
+                    j+=1
+                k+=1
+
+            while i < len(left_half):
+                tank_arr[k] = left_half[i]
+                i+=1
+                k+=1
+
+            while j < len(right_half):
+                tank_arr[k] = right_half[j]
+                j+=1
+                k+=1
