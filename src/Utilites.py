ca951f5a5ddeef29385e6ac42874c585c47d0b4f
nerdstat <ryan.jones178@gmail.com>
2020-02-10 15:22:14 -0500

    Add angle

--- src/Utilites.py
+++ src/Utilites.py
@@ -7,3 +7,6 @@ def get_filename(relpath):
 
 def get_distance(tank, tank1):
     return math.sqrt((tank1.xcor()-tank.xcor())**2 + (tank1.ycor()- tank.ycor())**2)
+
+def get_angle(tank, tank1):
+    return math.degrees(math.asin(abs(tank1.ycor() - tank.ycor())/(abs(get_distance(tank,tank1)))))
